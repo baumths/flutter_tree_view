6b354504446253f2ce6ae95ab426c740a778c1e9
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-03-31 19:00:25 -0300

    Simplifies NodeWidget

--- lib/src/node_widget.dart
+++ lib/src/node_widget.dart
@@ -1,212 +1,77 @@
 import 'internal.dart';
 
-/// A wrapper around [ListTile].
+/// A Simple widget to display [TreeNode]s in the [TreeView].
 ///
-/// Most theming properties were moved to [TreeViewTheme].
+/// Includes the indentation of nodes, the addition of Lines (if applicable) and
+/// the leading [NodeWidgetLeadingIcon] that already expands/collapses the
+/// node and animates itself.
 ///
-/// Includes the indentation of nodes, the addition of Lines (if applicable),
-/// the [ExpandNodeIcon] to the right of the Widget that already
-/// expand/collapse the node and animate itself.
+/// This widget will be wrapped in a [ScopedTreeNode] (an inherited widget) to
+/// give access to it's [TreeNode].
 ///
-/// Take a look at the example in the
-/// [online demo](https://mbaumgartenbr.github.io/flutter_tree_view).
-class NodeWidget extends StatefulWidget {
+/// Take a look at the [online demo](https://mbaumgartenbr.github.io/flutter_tree_view).
+class NodeWidget extends StatelessWidget {
   /// Creates a [NodeWidget].
+  ///
+  /// Notice that the [leading] [NodeWidgetLeadingIcon] used is responsible for
+  /// expanding/collapsing the node, if changed, the node would need to be
+  /// manually toggled through either [TreeViewController] or [ScopedTreeNode].
+  ///
+  /// Take a look at [ExpandNodeIcon] that has a similar implementation.
   const NodeWidget({
     Key? key,
-    required this.node,
-    required this.theme,
-    required this.title,
-    this.useExpandNodeIcon = true,
-    this.trailing = const [],
-    this.contentPadding,
-    this.horizontalTitleGap = 0,
-    this.dense,
+    this.content,
+    this.rowMainAxisSize = MainAxisSize.min,
+    this.leading = const NodeWidgetLeadingIcon(),
     this.onTap,
-    this.onToggle,
     this.onLongPress,
   }) : super(key: key);
 
-  /// The node to be displayed by this widget.
-  final TreeNode node;
-
-  /// The theme to be used for this widget.
-  final TreeViewTheme theme;
-
-  /// The Widget to be used as `title` of [ListTile].
+  /// A list of widgets to display to the right of [leading].
   ///
-  /// Usually a [Text] widget.
-  final Widget title;
+  /// This list will be added to the children of a [Row] widget.
+  ///
+  /// If left null, a [Text] widget with [TreeNode.label] will be used instead.
+  final List<Widget>? content;
 
-  /// If set to `false`, [ExpandNodeIcon] will be removed
-  /// from the trailing of [ListTile].
-  final bool useExpandNodeIcon;
+  /// The widget used to expand/collapse nodes.
+  ///
+  /// Defaults to [NodeWidgetLeadingIcon].
+  ///
+  /// Take a look at [ExpandNodeIcon] as an alternative leading.
+  final Widget leading;
 
-  /// List of items to display in a [Row]
-  /// before [ExpandNodeIcon] inside [ListTile.trailing]
-  final List<Widget> trailing;
+  /// The [Row.mainAxisSize] defaults to `MainAxisSize.min`.
+  final MainAxisSize rowMainAxisSize;
 
-  /// The tile's internal padding. (Doesn't affect lines space)
-  ///
-  /// Insets a [ListTile]'s contents: its [leading], [title],
-  /// [subtitle], and [trailing] widgets.
-  ///
-  /// `Copied from [ListTile.contentPadding].`
-  ///
-  /// If null, `EdgeInsets.zero` is used.
-  final EdgeInsetsGeometry? contentPadding;
-
-  /// The horizontal gap between the titles and the leading/trailing widgets.
-  ///
-  /// Defaults to 0.
-  final double horizontalTitleGap;
-
-  /// Whether this list tile is part of a vertically dense list.
-  ///
-  /// If this property is null then its value is based on [ListTileTheme.dense].
-  ///
-  /// Dense list tiles default to a smaller height.
-  ///
-  /// `Copied from [ListTile.dense].`
-  final bool? dense;
-
-  /// Callback for when user taps on a node.
+  /// Callback fired when the user taps on this [NodeWidget].
   final VoidCallback? onTap;
 
-  /// Callback for when a node is expanded/collapsed.
-  final VoidCallback? onToggle;
-
-  /// Callback for when user long presses a node.
+  /// Callback fired when the user long presses this [NodeWidget].
   final VoidCallback? onLongPress;
 
   @override
-  _NodeWidgetState createState() => _NodeWidgetState();
-}
-
-class _NodeWidgetState extends State<NodeWidget> {
-  late double indentation;
-
-  /// Calculates the indentation of [node] for the current [theme.lineStyle].
-  double calculateIndentation() {
-    var indentation = widget.node.calculateIndentation(widget.theme.indent);
-
-    if (widget.theme.lineStyle == LineStyle.connected) {
-      indentation += widget.theme.indent;
-    }
-    return indentation;
-  }
-
-  /// Updates the view.
-  void update() => setState(() {});
-
-  @override
-  void initState() {
-    super.initState();
-    widget.node.addUpdateCallback(update);
-    indentation = calculateIndentation();
-  }
-
-  @override
-  void didUpdateWidget(covariant NodeWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    indentation = calculateIndentation();
-  }
-
-  @override
-  void dispose() {
-    widget.node.removeUpdateCallback();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      minLeadingWidth: indentation,
-      horizontalTitleGap: widget.horizontalTitleGap,
-      contentPadding: widget.contentPadding ?? EdgeInsets.zero,
-      selected: widget.node.isSelected,
-      enabled: widget.node.isEnabled,
-      dense: widget.dense,
-      tileColor: widget.theme.nodeTileColor,
-      selectedTileColor: widget.theme.nodeSelectedTileColor,
-      hoverColor: widget.theme.nodeHoverColor,
-      focusColor: widget.theme.nodeFocusColor,
-      shape: widget.theme.nodeShape,
-      onTap: widget.onTap,
-      onLongPress: widget.onLongPress,
-      title: widget.title,
-      trailing: _buildTrailing(),
-      leading: LinesWidget(
-        node: widget.node,
-        theme: widget.theme,
-      ),
-    );
-  }
+    final scopedTreeNode = ScopedTreeNode.of(context);
 
-  Widget? _buildTrailing() {
-    if (widget.trailing.isEmpty) {
-      return widget.useExpandNodeIcon && widget.node.hasChildren
-          ? _expandIcon
-          : null;
-    }
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ...widget.trailing,
-        if (widget.node.hasChildren && widget.useExpandNodeIcon) _expandIcon,
-      ],
-    );
-  }
-
-  Widget get _expandIcon {
-    return ExpandNodeIcon(
-      node: widget.node,
-      onToggle: widget.onToggle,
-    );
-  }
-}
-
-/// Widget responsible for indenting nodes and drawing lines (if enabled).
-class LinesWidget extends StatelessWidget {
-  /// Creates a [LinesWidget].
-  const LinesWidget({
-    Key? key,
-    required this.node,
-    required this.theme,
-  }) : super(key: key);
-
-  /// The node to draw lines for.
-  final TreeNode node;
-
-  /// The theme to use while drawing lines.
-  final TreeViewTheme theme;
-
-  /// Decides on which type of lines to draw for
-  /// [node] based on [theme.lineStyle].
-  Widget chooseLines({required Widget child}) {
-    switch (theme.lineStyle) {
-      case LineStyle.scoped:
-        return CustomPaint(
-          painter: LinesPainter.scoped(node: node, theme: theme),
-          child: child,
-        );
-      case LineStyle.connected:
-        return CustomPaint(
-          painter: LinesPainter.connected(node: node, theme: theme),
-          child: child,
-        );
-      case LineStyle.disabled:
-      default:
-        return child;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return chooseLines(
-      child: SizedBox(
-        width: node.calculateIndentation(theme.indent),
-        height: double.infinity,
+    return InkWell(
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: Row(
+        mainAxisSize: rowMainAxisSize,
+        children: [
+          const LinesWidget(),
+          leading,
+          if (content == null)
+            Flexible(
+              child: Text(
+                scopedTreeNode.node.label,
+                style: Theme.of(context).textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ...?content,
+        ],
       ),
     );
   }
