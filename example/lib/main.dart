3412c088c1cb9358c776dd5f1a563b80bccc941a
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-03-31 19:06:20 -0300

    Example: Updates example with new API

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fancy_tree_view/flutter_fancy_tree_view.dart';
 
+import 'src/sample_data.dart';
 import 'src/home_page.dart';
 
-void main() => runApp(MaterialApp(home: HomePage()));
+void main() => runApp(MyApp());
+
+/// Recursively convert a list of maps into a list of [TreeNode]s.
+List<TreeNode> generateTreeNodes(List<Map<String, dynamic>> children) {
+  if (children.isEmpty) return const [];
+
+  return children.map((child) {
+    return TreeNode(
+      id: '${child['id'] ?? ''}',
+      label: child['name'] ?? '',
+    )..addChildren(generateTreeNodes(child['children'] ?? const []));
+  }).toList(growable: false);
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TreeViewController _treeController;
+
+  TreeNode buildTreeStructure() {
+    return TreeNode(id: '🌲️ ROOT')..addChildren(generateTreeNodes(sampleData));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _treeController = TreeViewController(
+      rootNode: buildTreeStructure(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _treeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TreeView Example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: kDarkBlue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        iconTheme: const IconThemeData(color: kDarkBlue),
+      ),
+      home: HomePage(treeController: _treeController),
+    );
+  }
+}
--- example/lib/src/sample_data.dart
+++ example/lib/src/sample_data.dart
@@ -1,79 +1,163 @@
-/// Sample data for [TreeView] demonstration
-const sampleData = <Map<String, dynamic>>[
+/// Sample data for [TreeView] example.
+const sampleData = <Map<String, Object>>[
   {
+    'id': 1,
     'name': '1',
-    'children': <Map<String, dynamic>>[
-      {'name': '1.1', 'children': <Map<String, dynamic>>[]},
+    'children': <Map<String, Object>>[
       {
+        'id': 11,
+        'name': '1.1',
+        'children': <Map<String, Object>>[],
+      },
+      {
+        'id': 12,
         'name': '1.2',
-        'children': <Map<String, dynamic>>[
-          {'name': '1.2.1', 'children': <Map<String, dynamic>>[]},
+        'children': <Map<String, Object>>[
+          {
+            'id': 121,
+            'name': '1.2.1',
+            'children': <Map<String, Object>>[],
+          },
         ]
       },
     ],
   },
   {
+    'id': 2,
     'name': '2',
-    'children': <Map<String, dynamic>>[
+    'children': <Map<String, Object>>[
       {
+        'id': 21,
         'name': '2.1',
-        'children': <Map<String, dynamic>>[
+        'children': <Map<String, Object>>[
           {
+            'id': 211,
             'name': '2.1.1',
-            'children': <Map<String, dynamic>>[
+            'children': <Map<String, Object>>[
               {
+                'id': 2111,
                 'name': '2.1.1.1',
-                'children': <Map<String, dynamic>>[
-                  {'name': '2.1.1.1.1', 'children': <Map<String, dynamic>>[]},
-                  {'name': '2.1.1.1.2', 'children': <Map<String, dynamic>>[]},
+                'children': <Map<String, Object>>[
+                  {
+                    'id': 21111,
+                    'name': '2.1.1.1.1',
+                    'children': <Map<String, Object>>[],
+                  },
+                  {
+                    'id': 21112,
+                    'name': '2.1.1.1.2',
+                    'children': <Map<String, Object>>[],
+                  },
                 ]
               },
-              {'name': '2.1.1.2', 'children': <Map<String, dynamic>>[]},
+              {
+                'id': 2112,
+                'name': '2.1.1.2',
+                'children': <Map<String, Object>>[],
+              },
             ]
           },
-          {'name': '2.1.2', 'children': <Map<String, dynamic>>[]},
-          {'name': '2.1.3', 'children': <Map<String, dynamic>>[]},
+          {
+            'id': 212,
+            'name': '2.1.2',
+            'children': <Map<String, Object>>[],
+          },
+          {
+            'id': 213,
+            'name': '2.1.3',
+            'children': <Map<String, Object>>[],
+          },
         ],
       },
       {
+        'id': 22,
         'name': '2.2',
-        'children': <Map<String, dynamic>>[
-          {'name': '2.2.1', 'children': <Map<String, dynamic>>[]},
-          {'name': '2.2.2', 'children': <Map<String, dynamic>>[]},
-          {'name': '2.2.3', 'children': <Map<String, dynamic>>[]},
+        'children': <Map<String, Object>>[
+          {
+            'id': 221,
+            'name': '2.2.1',
+            'children': <Map<String, Object>>[],
+          },
+          {
+            'id': 222,
+            'name': '2.2.2',
+            'children': <Map<String, Object>>[],
+          },
+          {
+            'id': 223,
+            'name': '2.2.3',
+            'children': <Map<String, Object>>[],
+          },
         ],
       },
     ],
   },
   {
+    'id': 3,
     'name': '3',
-    'children': <Map<String, dynamic>>[
+    'children': <Map<String, Object>>[
       {
+        'id': 31,
         'name': '3.1',
         'children': [
           {
+            'id': 311,
             'name': '3.1.1',
-            'children': <Map<String, dynamic>>[
+            'children': <Map<String, Object>>[
               {
+                'id': 3111,
                 'name': '3.1.1.1',
-                'children': <Map<String, dynamic>>[
-                  {'name': '3.1.1.1.1', 'children': <Map<String, dynamic>>[]},
-                  {'name': '3.1.1.1.2', 'children': <Map<String, dynamic>>[]},
-                  {'name': '3.1.1.1.3', 'children': <Map<String, dynamic>>[]}
+                'children': <Map<String, Object>>[
+                  {
+                    'id': 31111,
+                    'name': '3.1.1.1.1',
+                    'children': <Map<String, Object>>[],
+                  },
+                  {
+                    'id': 31112,
+                    'name': '3.1.1.1.2',
+                    'children': <Map<String, Object>>[],
+                  },
+                  {
+                    'id': 31113,
+                    'name': '3.1.1.1.3',
+                    'children': <Map<String, Object>>[],
+                  }
                 ]
               },
             ]
           },
-          {'name': '3.1.2', 'children': <Map<String, dynamic>>[]},
-          {'name': '3.1.3', 'children': <Map<String, dynamic>>[]},
+          {
+            'id': 312,
+            'name': '3.1.2',
+            'children': <Map<String, Object>>[],
+          },
+          {
+            'id': 313,
+            'name': '3.1.3',
+            'children': <Map<String, Object>>[],
+          },
         ],
       },
       {
+        'id': 32,
         'name': '3.2',
-        'children': <Map<String, dynamic>>[
-          {'name': '3.2.1', 'children': <Map<String, dynamic>>[]},
-          {'name': '3.2.2', 'children': <Map<String, dynamic>>[]},
-          {'name': '3.2.3', 'children': <Map<String, dynamic>>[]},
+        'children': <Map<String, Object>>[
+          {
+            'id': 321,
+            'name': '3.2.1',
+            'children': <Map<String, Object>>[],
+          },
+          {
+            'id': 322,
+            'name': '3.2.2',
+            'children': <Map<String, Object>>[],
+          },
+          {
+            'id': 323,
+            'name': '3.2.3',
+            'children': <Map<String, Object>>[],
+          },
         ],
       },
     ],
