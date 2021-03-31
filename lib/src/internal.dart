3cd3ca36b4088ac14c4a31de997ad969d506396e
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-03-31 19:03:52 -0300

    Fixes file exports

--- lib/src/internal.dart
+++ lib/src/internal.dart
@@ -2,7 +2,10 @@ export 'package:flutter/material.dart';
 
 export 'expand_node_icon.dart';
 export 'lines_painter.dart';
+export 'lines_widget.dart';
+export 'node_widget_leading_icon.dart';
 export 'node_widget.dart';
+export 'scoped_tree_node.dart';
 export 'tree_node.dart';
 export 'tree_view_controller.dart';
 export 'tree_view_theme.dart';
