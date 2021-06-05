38227a789af0cb8a598b3a477c4bd27757ec500c
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-06-05 09:07:29 -0300

    Adds new refresh Feature
    TreeViewController received two new public methods:
      * refreshNode
      * reset
    And two new internal:
      * shouldRefresh
      * nodeRefreshed
    TreeNodeScope now has an internal 'shouldRefresh'.

--- lib/src/tree_view_controller.dart
+++ lib/src/tree_view_controller.dart
@@ -4,6 +4,11 @@ import 'package:flutter/foundation.dart';
 
 import 'tree_node.dart';
 
+// TODO: |
+//* Perhaps it'd be easy to setup an "indexOfNodeMap" to store node's index
+//* to avoid having to call _visibleNodes.indexOf(TreeNode) as we are using
+//* indices (indexOf(parent) + 1) to add nodes to visibleNodes.
+
 /// A simple Controller for managing the nodes that compose [TreeView].
 ///
 /// This class was extracted from [TreeView] for use cases where you
@@ -66,6 +71,9 @@ class TreeViewController extends TreeViewControllerBase with ChangeNotifier {
   /// );
   /// ```
   /// No checks are done to [node] before calling this method, i.e. [isExpanded].
+  ///
+  /// Make sure this callback is synchronous, if you need to add child nodes
+  /// asynchronously, use [refreshNode] instead.
   final void Function(TreeNode node)? onAboutToExpand;
 
   /// Cache to avoid searching multiple times for the same node.
@@ -88,6 +96,24 @@ class TreeViewController extends TreeViewControllerBase with ChangeNotifier {
 
     return searchedNode;
   }
+
+  // * ~~~~~~~~~~ INTERNAL REFRESH ~~~~~~~~~~ *
+
+  /// A map that keeps track of which [TreeNode]'s widget needs to be rebuilt.
+  late final _nodesThatShouldRefresh = <String, bool>{};
+
+  /// Checks if the [TreeNode] with [id] needs to be refreshed (update lines, ...).
+  ///
+  /// A [TreeNode] that is marked as needing refresh will only rebuild itself,
+  /// to rebuild an entire subtree, use [refreshNode].
+  ///
+  /// This is currently only used to update the lines of a node when it's sibling
+  /// list changes. (If nodes are kept expanded when a new sibling is added,
+  /// the lines are not updated and the new node lines are not connected.)
+  bool shouldRefresh(String id) => _nodesThatShouldRefresh[id] ?? false;
+
+  /// Removes [id] from the map of nodes that needs refresh.
+  void nodeRefreshed(String id) => _nodesThatShouldRefresh.remove(id);
 
   // * ~~~~~~~~~~ EXPAND/COLLAPSE METHODS ~~~~~~~~~~ *
 
@@ -141,6 +167,37 @@ class TreeViewController extends TreeViewControllerBase with ChangeNotifier {
     super.toggleExpanded(node);
     notifyListeners();
   }
+
+  /// Refreshes [node]'s subtree.
+  ///
+  /// Useful when [node.children] has changed. But be careful, this method could
+  /// slow down your view as it collapses and re-expands the nodes.
+  ///
+  /// If [node] is not expanded, nothing happens.
+  ///
+  /// Set [keepExpandedNodes] to `true` if you want to preserve the expansion
+  /// state of the subtree of [node].
+  @override
+  void refreshNode(TreeNode node, {bool keepExpandedNodes = false}) {
+    if (node.hasChildren) {
+      node.children.forEach(
+        (child) => _nodesThatShouldRefresh[child.id] = true,
+      );
+    }
+
+    super.refreshNode(node, keepExpandedNodes: keepExpandedNodes);
+    notifyListeners();
+  }
+
+  /// Resets the entire state of this controller and populates [visibleNodes]
+  /// with the children of [rootNode].
+  ///
+  /// Useful when a top level node needs to be deleted.
+  @override
+  void reset({bool keepExpandedNodes = false}) {
+    super.reset(keepExpandedNodes: keepExpandedNodes);
+    notifyListeners();
+  }
 }
 
 /// Base implementation of [TreeViewController].
@@ -155,16 +212,7 @@ class TreeViewControllerBase {
     required this.rootNode,
     this.useBinarySearch = false,
   }) {
-    _visibleNodes.clear();
-    _visibleNodesMap.clear();
-    _expandedNodes.clear();
-
-    rootNode.children.forEach((child) {
-      _visibleNodes.add(child);
-      _visibleNodesMap[child.id] = true;
-    });
-
-    _expandedNodes[rootNode.id] = true;
+    _populateInitialNodes();
   }
 
   /// Whether [TreeViewControllerBase.indexOf] should use flutter's [binarySearch]
@@ -226,17 +274,19 @@ class TreeViewControllerBase {
   /// If the path from root to [node] has collapsed nodes, it will expand them too.
   @mustCallSuper
   void expandNode(TreeNode node) {
-    if (node.isRoot || isExpanded(node.id)) return;
+    if (isExpanded(node.id)) return;
 
-    // Expand all ancestors of [node] if its parent is not expanded.
-    if (!isExpanded(node.parent!.id)) expandUntil(node);
+    if (!node.isRoot && !isExpanded(node.parent!.id)) {
+      // Expand all ancestors of [node] if its parent is not expanded.
+      expandUntil(node);
+    }
 
     _expandedNodes[node.id] = true;
 
     if (node.hasChildren) {
       var index = indexOf(node);
 
-      node.children.forEach((child) {
+      node.children.forEach((TreeNode child) {
         if (isVisible(child.id)) return;
 
         index++;
@@ -252,13 +302,12 @@ class TreeViewControllerBase {
   void collapseNode(TreeNode node) {
     if (!isExpanded(node.id)) return;
 
-    if (!node.isRoot) {
-      _expandedNodes.remove(node.id);
-    }
+    _expandedNodes.remove(node.id);
 
     node.descendants.forEach((descendant) {
-      if (descendant.isRemovable && isVisible(descendant.id)) {
-        _expandedNodes.remove(descendant.id);
+      _expandedNodes.remove(descendant.id);
+
+      if (descendant.isRemovable) {
         _visibleNodes.remove(descendant);
         _visibleNodesMap.remove(descendant.id);
       }
@@ -282,5 +331,76 @@ class TreeViewControllerBase {
   @mustCallSuper
   void expandUntil(TreeNode node) {
     node.ancestors.forEach(expandNode);
+  }
+
+  /// Refreshes [node]'s subtree. Useful when [node.children] has changed.
+  ///
+  /// Make sure to do atomic refreshes whenever possible, refreshing the whole
+  /// tree might hurt performance in large trees.
+  ///
+  /// If [node] is not expanded, nothing happens.
+  ///
+  /// Set [keepExpandedNodes] to `true` if you want to preserve the expansion
+  /// state of the subtree of [node].
+  @mustCallSuper
+  void refreshNode(TreeNode node, {bool keepExpandedNodes = false}) {
+    if (node == rootNode) return reset(keepExpandedNodes: keepExpandedNodes);
+
+    if (!isExpanded(node.id)) return;
+
+    List<TreeNode>? previouslyExpandedNodes;
+
+    if (keepExpandedNodes) {
+      previouslyExpandedNodes = node.descendants
+          .where((descendant) => isExpanded(descendant.id))
+          .toList(growable: false);
+    }
+
+    collapseNode(node);
+    _pruneDirtyNodes();
+    expandNode(node);
+
+    previouslyExpandedNodes?.forEach(expandNode);
+  }
+
+  /// Resets the entire state of this controller and populates [visibleNodes]
+  /// with the children of [rootNode].
+  ///
+  /// Useful when a top level node needs to be deleted.
+  @mustCallSuper
+  void reset({bool keepExpandedNodes = false}) {
+    List<TreeNode>? previouslyExpandedNodes;
+
+    if (keepExpandedNodes) {
+      previouslyExpandedNodes = rootNode.descendants
+          .where((descendant) => isExpanded(descendant.id))
+          .toList(growable: false);
+    }
+
+    _visibleNodes.clear();
+    _visibleNodesMap.clear();
+    _expandedNodes.clear();
+
+    _populateInitialNodes();
+
+    previouslyExpandedNodes?.forEach(expandNode);
+  }
+
+  /// Adds the children of [rootNode] to [_visibleNodes].
+  void _populateInitialNodes() {
+    rootNode.children.forEach((child) {
+      _visibleNodes.add(child);
+      _visibleNodesMap[child.id] = true;
+    });
+
+    _expandedNodes[rootNode.id] = true;
+  }
+
+  /// No node with `parent == null` should be displayed on the tree.
+  ///
+  /// The only node allowed to have parent = null is [rootNode], which must not
+  /// be displayed, it's only used to index other nodes.
+  void _pruneDirtyNodes() {
+    _visibleNodes.removeWhere((node) => node.isRoot);
   }
 }
