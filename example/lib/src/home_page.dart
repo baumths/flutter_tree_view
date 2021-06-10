6d16049bf5b7d3a55f3ab7b9fd4103f455117324
mbaumgartenbr <mbaumgartenbr@gmail.com>
2021-06-10 17:23:49 -0300

    Example: Updates Example Application
    Redesigned the entire UI to use most of the TreeView Features

--- example/lib/src/home_page.dart
+++ example/lib/src/home_page.dart
@@ -1,224 +1,62 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_fancy_tree_view/flutter_fancy_tree_view.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-part 'home_utils.dart';
+import 'settings/settings_view.dart';
+import 'custom_tree_view.dart';
 
-const kDarkBlue = Color(0xFF1565C0);
+const String url = '''
+https://github.com/mbaumgartenbr/flutter_tree_view/tree/main/example
+''';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.treeController}) : super(key: key);
-
-  final TreeViewController treeController;
-
-  @override
-  _HomePageState createState() => _HomePageState();
+void _launchExampleSourceCode() {
+  canLaunch(url).then((bool canLaunch) {
+    if (canLaunch) launch(url);
+  });
 }
 
-class _HomePageState extends State<HomePage> {
-  static const double nodeHeight = 48.0;
-
-  late ScrollController scrollController;
-
-  late TreeViewController treeController;
-
-  late var treeTheme = const TreeViewTheme();
-
-  late Widget _nodeIcon = const NodeWidgetLeadingIcon(
-    useFoldersOnly: true,
-    leafIconDisabledColor: kDarkBlue,
-  );
-
-  late final _dynamicController = TreeViewController(
-    onAboutToExpand: (TreeNode nodeAboutToExpand) {
-      if (treeController != _dynamicController) return;
-
-      final children = _dynamicChildrenMap[nodeAboutToExpand.id];
-
-      if (children != null) {
-        nodeAboutToExpand.addChildren(
-          children.map((child) => TreeNode(id: child, label: child)),
-        );
-      }
-    },
-    rootNode: TreeNode(id: 'Root')
-      //? Initial Dynamic Nodes
-      ..addChild(TreeNode(id: 'A', label: 'A'))
-      ..addChild(TreeNode(id: 'B', label: 'B'))
-      ..addChild(TreeNode(id: 'C', label: 'C')),
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    scrollController = ScrollController();
-    treeController = widget.treeController;
-  }
-
-  @override
-  void dispose() {
-    _dynamicController.dispose();
-    scrollController.dispose();
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _isSmallDisplay = MediaQuery.of(context).size.width < 600;
+
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: SizedBox(
-                width: 600,
-                child: TreeView(
-                  nodeHeight: nodeHeight,
-                  controller: treeController,
-                  scrollController: scrollController,
-                  theme: treeTheme,
-                  nodeBuilder: (_, TreeNode treeNode) => NodeWidget(
-                    leading: _nodeIcon,
-                    onLongPress: () => _describeAncestors(treeNode),
-                    onTap: () => showSnackBar(
-                      context,
-                      treeNode.toString(),
-                      duration: const Duration(seconds: 3),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 24,
-            right: 80,
-            child: _LineThicknessSlider(
-              onChanged: _lineThicknessChanged,
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: _SpeedDial(
-        controller: treeController,
-        changeLineStyle: _changeLineStyle,
-        changeNodeIcon: _changeNodeIcon,
-        changeTreeController: _changeController,
-      ),
+      body: const _ResponsiveBody(),
+      endDrawer: const Drawer(child: SettingsView()),
       appBar: AppBar(
-        title: const Center(child: Text('TreeView Example')),
-        actions: [
-          IconButton(
-            tooltip: 'REVEAL NODE',
-            icon: const Icon(Icons.search_rounded),
-            onPressed: _revealNodeDialog,
-          ),
-          const SizedBox(width: 16),
-        ],
+        centerTitle: true,
+        title: const Text('TreeView Example'),
+        leading: const IconButton(
+          tooltip: 'SOURCE CODE',
+          icon: Icon(Icons.open_in_new_rounded),
+          onPressed: _launchExampleSourceCode,
+        ),
+        actions: _isSmallDisplay ? null : const [SizedBox()],
       ),
-    );
-  }
-
-  void _changeController() {
-    if (treeController == widget.treeController) {
-      setState(() {
-        treeTheme = treeTheme.copyWith(roundLineCorners: true);
-        treeController = _dynamicController;
-      });
-    } else {
-      setState(() {
-        treeTheme = treeTheme.copyWith(roundLineCorners: false);
-        treeController = widget.treeController;
-      });
-    }
-  }
-
-  void _revealNodeDialog() async {
-    final nodeId = await showDialog<String>(
-      context: context,
-      builder: (context) => const Dialog(child: FindNodeDialog()),
-    );
-    if (nodeId == null) return;
-
-    final node = treeController.find(nodeId);
-
-    if (node != null) {
-      treeController.expandUntil(node);
-
-      final offsetOfNode = treeController.indexOf(node) * nodeHeight;
-
-      await scrollController.animateTo(
-        offsetOfNode,
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.fastOutSlowIn,
-      );
-    } else {
-      showSnackBar(context, "Couldn't find a TreeNode with ID = $nodeId");
-    }
-  }
-
-  void _changeNodeIcon() {
-    if (_nodeIcon is NodeWidgetLeadingIcon) {
-      setState(() {
-        _nodeIcon = const ExpandNodeIcon(
-          color: kDarkBlue,
-          disabledColor: Colors.grey,
-        );
-      });
-    } else {
-      setState(() {
-        _nodeIcon = const NodeWidgetLeadingIcon(
-          useFoldersOnly: true,
-          leafIconDisabledColor: kDarkBlue,
-        );
-      });
-    }
-  }
-
-  void _lineThicknessChanged(double value) {
-    if (treeTheme.lineThickness != value) {
-      setState(() {
-        treeTheme = treeTheme.copyWith(lineThickness: value);
-      });
-    }
-  }
-
-  void _changeLineStyle() {
-    switch (treeTheme.lineStyle) {
-      case LineStyle.disabled:
-        setState(() {
-          treeTheme = treeTheme.copyWith(lineStyle: LineStyle.connected);
-        });
-        break;
-      case LineStyle.connected:
-        setState(() {
-          treeTheme = treeTheme.copyWith(lineStyle: LineStyle.scoped);
-        });
-        break;
-      case LineStyle.scoped:
-        setState(() {
-          treeTheme = treeTheme.copyWith(lineStyle: LineStyle.disabled);
-        });
-        break;
-    }
-  }
-
-  void _describeAncestors(TreeNode node) {
-    final ancestors = node.ancestors.map((e) => e.id).join('/');
-
-    showSnackBar(
-      context,
-      'Path of "${node.label}": /$ancestors/${node.id}',
-      duration: const Duration(seconds: 3),
     );
   }
 }
 
-const _dynamicChildrenMap = <String, List<String>>{
-  'A': ['A1'],
-  'C': ['C1', 'C2', 'C3', 'C4', 'C5'],
-  'C2': ['C21'],
-  'C4': ['C41', 'C42'],
-  'C41': ['C411'],
-};
+class _ResponsiveBody extends StatelessWidget {
+  const _ResponsiveBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).size.width < 600) {
+      return const CustomTreeView();
+    }
+    return Row(
+      children: const [
+        Flexible(flex: 2, child: CustomTreeView()),
+        VerticalDivider(
+          width: 2,
+          thickness: 2,
+          color: Colors.black26,
+        ),
+        Expanded(child: SettingsView()),
+      ],
+    );
+  }
+}
