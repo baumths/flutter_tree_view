c5787bd9d724d07cfcbd0709a6ba5e9e5f9640e9
mbaumgartenbr <mbaumgartenbr@gmail.com>
2022-09-27 17:02:09 -0300

    Add SliverTree and TreeView

---
+++ lib/src/widgets/sliver_tree.dart
@@ -0,0 +1,447 @@
+import 'dart:collection' show UnmodifiableSetView;
+import 'dart:math' as math show min;
+
+import 'package:flutter/material.dart';
+
+import '../foundation.dart';
+
+/// Signature for a function that creates a widget for a given entry, e.g., in a
+/// tree.
+typedef TreeViewItemBuilder<T extends Object> = Widget Function(
+  BuildContext context,
+  TreeEntry<T> entry,
+);
+
+/// Signature for a function that takes a widget and an animation to apply
+/// transitions if needed.
+typedef TreeViewTransitionBuilder = Widget Function(
+  BuildContext context,
+  Widget child,
+  Animation<double> animation,
+);
+
+/// The default transition builder used by [SliverTree] to animate the expansion
+/// state changes of a node.
+///
+/// Wraps [child] in [SizeTransition].
+Widget defaultTreeViewTransitionBuilder(
+  BuildContext context,
+  Widget child,
+  Animation<double> animation,
+) {
+  return SizeTransition(
+    sizeFactor: animation,
+    child: child,
+  );
+}
+
+/// A widget that wraps a [SliverList] adding tree viewing capabilities.
+class SliverTree<T extends Object> extends StatefulWidget {
+  /// Creates a [SliverTree].
+  const SliverTree({
+    super.key,
+    required this.itemBuilder,
+    required this.controller,
+    this.transitionBuilder = defaultTreeViewTransitionBuilder,
+    this.maxNodesToShowWhenAnimating = 50,
+  });
+
+  /// The controller responsible for managing the expansion state and animations
+  /// of the tree provided by its [TreeController.tree].
+  final TreeController<T> controller;
+
+  /// Callback used to map your data into widgets.
+  ///
+  /// The `TreeEntry<T> entry` parameter contains important information about
+  /// the current tree context of the particular [TreeEntry.node] that it holds.
+  final TreeViewItemBuilder<T> itemBuilder;
+
+  /// Callback used to animate the expansion state changes of a subtree.
+  ///
+  /// See also:
+  ///
+  ///   * [defaultTreeViewTransitionBuilder] that uses a [SizeTransition].
+  final TreeViewTransitionBuilder transitionBuilder;
+
+  /// The amount of nodes that are going to be shown on an animating subtree.
+  ///
+  /// Must be a value greater than `0`, since the "pseudo root" of the animating
+  /// subtree should always be visible.
+  ///
+  /// When animating the expand/collapse state changes, all descendant nodes
+  /// whose visibility will change are rendered along with the toggled node,
+  /// i.e. a [Column] is used, therefore rendering the entire subtree regardless
+  /// of being a "lazy" rendered view.
+  ///
+  /// This value can be used to limit how many nodes are actually rendered
+  /// during the animation, since there could be cases where not all widgets
+  /// are visible due to scroll offsets.
+  ///
+  /// Defaults to `50`.
+  final int maxNodesToShowWhenAnimating;
+
+  /// The [SliverTree] from the closest instance of this class that encloses the
+  /// given context.
+  ///
+  /// If there is no [SliverTree] ancestor in the widget tree at the given
+  /// context, then this will return null.
+  ///
+  /// Typical usage is as follows:
+  ///
+  /// SliverTreeState<T>? treeState = SliverTree.maybeOf<T>(context);
+  ///
+  /// See also:
+  ///
+  ///  * [of], which will throw in debug mode if no [SliverTree] ancestor exists
+  ///    in the widget tree.
+  static SliverTreeState<T>? maybeOf<T extends Object>(BuildContext context) {
+    return context.findAncestorStateOfType<SliverTreeState<T>>();
+  }
+
+  /// The [SliverTree] from the closest instance of this class that encloses the
+  /// given context.
+  ///
+  /// If there is no [SliverTree] ancestor in the widget tree at the given
+  /// context, then this will throw in debug mode.
+  ///
+  /// Typical usage is as follows:
+  ///
+  /// ```dart
+  /// SliverTreeState<T> treeState = SliverTree.of<T>(context);
+  /// ```
+  ///
+  /// See also:
+  ///
+  ///  * [maybeOf], which will return null if no [SliverTree] ancestor exists in
+  ///    the widget tree.
+  static SliverTreeState<T> of<T extends Object>(BuildContext context) {
+    final SliverTreeState<T>? instance = maybeOf<T>(context);
+    assert(() {
+      if (instance == null) {
+        throw FlutterError.fromParts(<DiagnosticsNode>[
+          ErrorSummary(
+            'SliverTree.of() called with a context that does not contain a '
+            'SliverTree.',
+          ),
+          ErrorDescription(
+            'No SliverTree ancestor could be found starting from the context '
+            'that was passed to SliverTree.of().',
+          ),
+          ErrorHint(
+            'This can happen when the context provided is from the same '
+            'StatefulWidget that built the SliverTree.',
+          ),
+          context.describeElement('The context used was'),
+        ]);
+      }
+      return true;
+    }());
+    return instance!;
+  }
+
+  @override
+  State<SliverTree<T>> createState() => SliverTreeState<T>();
+}
+
+/// The object that holds the state of a [SliverTree].
+class SliverTreeState<T extends Object> extends State<SliverTree<T>> {
+  /// The controller responsible for managing the expansion state and animations
+  /// of the tree provided by its [TreeController.tree].
+  TreeController<T> get controller => widget.controller;
+
+  void _rebuild() => setState(() {});
+
+  //* REORDERING ---------------------------------------------------------------
+
+  Rect _autoScrollRect = Rect.zero;
+  VerticalEdgeDraggingAutoScroller? _autoScroller;
+
+  void _handleScrollableAutoScrolled() {
+    if (_autoScrollRect == Rect.zero) return;
+    // Continue scrolling if the drag is still in progress.
+    _autoScroller?.startAutoScrollIfNecessary(_autoScrollRect);
+  }
+
+  /// Starts scrolling the ancestor [Scrollable] if [rect] is close enough to
+  /// the vertical edges of the scrollable's viewport.
+  ///
+  /// Used by [TreeDraggable] to auto scroll when dragging.
+  void startAutoScrollIfNecessary(Rect rect) {
+    _autoScrollRect = rect;
+    _autoScroller?.startAutoScrollIfNecessary(_autoScrollRect);
+  }
+
+  /// Stops scrolling the ancestor [Scrollable].
+  ///
+  /// Used by [TreeDraggable] when stopped dragging.
+  void stopAutoScroll() {
+    _autoScroller?.stopAutoScroll();
+    _autoScrollRect = Rect.zero;
+  }
+
+  /// An unordered set of node ids composed by the ids of every ancestor of the
+  /// node that is currently being dragged (if any).
+  /// If no node is currenlty being dragged, returns an empty set.
+  ///
+  /// Used by [TreeDragTarget] to avoid collapsing the ancestors of a dragging
+  /// node.
+  UnmodifiableSetView<Object> get draggingNodePath => _draggingNodePath;
+  UnmodifiableSetView<Object> _draggingNodePath = UnmodifiableSetView(const {});
+
+  /// Called by [TreeDraggable] when it starts dragging to make sure that the
+  /// dragged node stays visible during the drag gesture by disabling auto
+  /// toggle expansion.
+  ///
+  /// This should not be used from outside of [TreeDraggable].
+  void onNodeDragStarted(TreeEntry<T> entry) {
+    final Set<Object> path = {entry.id};
+
+    TreeEntry<T>? current = entry.parent;
+    while (current != null) {
+      path.add(current.id);
+      current = current.parent;
+    }
+
+    _draggingNodePath = UnmodifiableSetView<Object>(path);
+  }
+
+  /// Called by [TreeDraggable] when it stops dragging to clear [draggingNodePath].
+  ///
+  /// This should not be used from outside of [TreeDraggable].
+  void onNodeDragEnded() {
+    _draggingNodePath = UnmodifiableSetView(const {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(_rebuild);
+
+    if (controller.flattenedTree.isEmpty) {
+      controller.rebuild();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant SliverTree<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.controller != controller) {
+      oldWidget.controller.removeListener(_rebuild);
+      controller.addListener(_rebuild);
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final ScrollableState scrollable = Scrollable.of(context)!;
+
+    if (_autoScroller?.scrollable != scrollable) {
+      _autoScroller?.stopAutoScroll();
+      _autoScroller = VerticalEdgeDraggingAutoScroller(
+        scrollable: scrollable,
+        onScrollViewScrolled: _handleScrollableAutoScrolled,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_rebuild);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        _itemBuilder,
+        childCount: controller.flattenedTree.length,
+      ),
+    );
+  }
+
+  Widget _keyedItemBuilder(BuildContext context, TreeEntry<T> entry) {
+    return KeyedSubtree(
+      key: _SaltedKey(entry.id),
+      child: widget.itemBuilder(context, entry),
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, int index) {
+    final TreeEntry<T> entry = controller.entryAt(index);
+
+    final AnimatableTreeCommand<T>? command =
+        controller.findAnimatableCommand(entry.node);
+
+    if (command == null) {
+      return _keyedItemBuilder(context, entry);
+    }
+
+    return _AnimatingSubTree<T>(
+      itemBuilder: _keyedItemBuilder,
+      transitionBuilder: widget.transitionBuilder,
+      maxNodesToShowWhenAnimating: widget.maxNodesToShowWhenAnimating,
+      animationDuration: command.duration,
+      animationCurve: command.curve,
+      startAnimating: command.animate,
+      onAnimationComplete: () {
+        controller.onAnimatableCommandComplete(entry.node);
+      },
+      tree: _SubTree<T>(
+        root: entry.node,
+        motherTree: controller.tree,
+        startingLevel: entry.level,
+        parentLineLevels: entry.ancestorLevelsWithVerticalLines,
+      ),
+    );
+  }
+}
+
+class _SaltedKey<T extends State<StatefulWidget>> extends GlobalObjectKey<T> {
+  const _SaltedKey(super.value);
+}
+
+class _SubTree<T extends Object> extends Tree<T> {
+  _SubTree({
+    required T root,
+    required this.motherTree,
+    required this.parentLineLevels,
+    required this.startingLevel,
+  }) : pseudoRoots = <T>[root];
+
+  final Tree<T> motherTree;
+
+  final int startingLevel;
+  final Set<int> parentLineLevels;
+
+  final List<T> pseudoRoots;
+
+  @override
+  List<T> get roots => pseudoRoots;
+
+  @override
+  Object getId(T node) => motherTree.getId(node);
+
+  @override
+  List<T> getChildren(T node) => motherTree.getChildren(node);
+
+  @override
+  bool getExpansionState(T node) => motherTree.getExpansionState(node);
+
+  @override
+  void setExpansionState(T node, bool expanded) {/* NO-OP */}
+}
+
+class _AnimatingSubTree<T extends Object> extends StatefulWidget {
+  const _AnimatingSubTree({
+    super.key,
+    required this.tree,
+    required this.itemBuilder,
+    required this.transitionBuilder,
+    required this.maxNodesToShowWhenAnimating,
+    required this.animationDuration,
+    required this.animationCurve,
+    required this.startAnimating,
+    required this.onAnimationComplete,
+  });
+
+  final _SubTree<T> tree;
+  final TreeViewItemBuilder<T> itemBuilder;
+  final TreeViewTransitionBuilder transitionBuilder;
+  final int maxNodesToShowWhenAnimating;
+  final Duration animationDuration;
+  final Curve animationCurve;
+  final TickerFuture Function(AnimationController controller) startAnimating;
+  final VoidCallback onAnimationComplete;
+
+  @override
+  State<_AnimatingSubTree<T>> createState() => _AnimatingSubTreeState<T>();
+}
+
+class _AnimatingSubTreeState<T extends Object>
+    extends State<_AnimatingSubTree<T>> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final CurvedAnimation _animation;
+
+  _SubTree<T> get tree => widget.tree;
+
+  late List<TreeEntry<T>> _branch;
+  late TreeEntry<T> _rootEntry;
+  late int _itemCount;
+
+  @override
+  void initState() {
+    super.initState();
+    _branch = tree.flatten(startingLevel: tree.startingLevel);
+    // at least the subtree "pseudo root" must be present
+    assert(_branch.isNotEmpty);
+    _rootEntry = _branch.first;
+
+    // Make sure to add the lines of the ancestors in the main tree, so that
+    // this subtree doesn't appear floating "contextless" in the line hierarchy.
+    //
+    // Adding the extra line levels to the root will ensure all descendants have
+    // it due to the recursive [TreeEntry.ancestorLevelsWithVerticalLines] calls.
+    _rootEntry.addVerticalLinesAtLevels(tree.parentLineLevels);
+
+    _itemCount = math.min(_branch.length, widget.maxNodesToShowWhenAnimating);
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.animationDuration,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: widget.animationCurve,
+    );
+
+    widget
+        .startAnimating(_controller)
+        .whenCompleteOrCancel(widget.onAnimationComplete);
+  }
+
+  @override
+  void didUpdateWidget(covariant _AnimatingSubTree<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.duration = widget.animationDuration;
+    _animation.curve = widget.animationCurve;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_itemCount == 1) {
+      return widget.itemBuilder(context, _rootEntry);
+    }
+
+    late final Widget descendants = IgnorePointer(
+      ignoring: true,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          for (int index = 1; index < _itemCount; index++)
+            widget.itemBuilder(context, _branch[index]),
+        ],
+      ),
+    );
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        widget.itemBuilder(context, _rootEntry),
+        widget.transitionBuilder(context, descendants, _animation),
+      ],
+    );
+  }
+}
---
+++ lib/src/widgets/tree_view.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/gestures.dart' show DragStartBehavior;
+import 'package:flutter/material.dart';
+
+import '../foundation.dart';
+import 'sliver_tree.dart';
+
+/// A simple and highly customizable hierarchy visualization widget.
+///
+/// See also:
+///
+/// * [SliverTree], which is created internally by [TreeView]. It can be used to
+///   create more sophisticated scrolling experineces.
+class TreeView<T extends Object> extends StatelessWidget {
+  /// Creates a [TreeView].
+  const TreeView({
+    super.key,
+    required this.itemBuilder,
+    required this.controller,
+    this.transitionBuilder = defaultTreeViewTransitionBuilder,
+    this.maxNodesToShowWhenAnimating = 50,
+    this.padding,
+    this.scrollController,
+    this.primary,
+    this.physics,
+    this.scrollBehavior,
+    this.shrinkWrap = false,
+    this.cacheExtent,
+    this.semanticChildCount,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+    this.restorationId,
+    this.clipBehavior = Clip.hardEdge,
+  }) : assert(maxNodesToShowWhenAnimating > 0);
+
+  /// The controller responsible for managing the expansion state and animations
+  /// of the tree provided by its [TreeController.tree].
+  final TreeController<T> controller;
+
+  /// Callback used to map your data into widgets.
+  ///
+  /// The `TreeEntry<T> entry` parameter contains important information about
+  /// the current tree context of the particular [TreeEntry.node] that it holds.
+  final TreeViewItemBuilder<T> itemBuilder;
+
+  /// Callback used to animate the expansion state change of a subtree.
+  ///
+  /// See also:
+  ///
+  /// * [defaultTreeViewTransitionBuilder] that uses a [SizeTransition].
+  final TreeViewTransitionBuilder transitionBuilder;
+
+  /// The amount of nodes that are going to be shown on an animating subtree.
+  ///
+  /// Must be a value greater than `0`, since the "pseudo root" of the animating
+  /// subtree should always be visible.
+  ///
+  /// When animating the expand/collapse state changes, all descendant nodes
+  /// whose visibility will change are rendered along with the toggled node,
+  /// i.e. a [Column] is used, therefore rendering the entire subtree regardless
+  /// of being a "lazy" rendered view.
+  ///
+  /// This value can be used to limit how many nodes are actually rendered
+  /// during the animation, since there could be cases where not all widgets
+  /// are visible due to scroll offsets.
+  ///
+  /// Defaults to `50`.
+  final int maxNodesToShowWhenAnimating;
+
+  /// {@macro flutter.widgets.scroll_view.controller}
+  final ScrollController? scrollController;
+
+  /// {@macro flutter.widgets.scroll_view.primary}
+  final bool? primary;
+
+  /// {@macro flutter.widgets.scroll_view.physics}
+  final ScrollPhysics? physics;
+
+  /// {@macro flutter.widgets.shadow.scrollBehavior}
+  ///
+  /// [ScrollBehavior]s also provide [ScrollPhysics]. If an explicit
+  /// [ScrollPhysics] is provided in [physics], it will take precedence,
+  /// followed by [scrollBehavior], and then the inherited ancestor
+  /// [ScrollBehavior].
+  final ScrollBehavior? scrollBehavior;
+
+  /// {@macro flutter.widgets.scroll_view.shrinkWrap}
+  final bool shrinkWrap;
+
+  /// The amount of space by which to inset the tree contents.
+  ///
+  /// It defaults to `EdgeInsets.zero`.
+  final EdgeInsetsGeometry? padding;
+
+  /// {@macro flutter.rendering.RenderViewportBase.cacheExtent}
+  final double? cacheExtent;
+
+  /// The number of children that will contribute semantic information.
+  ///
+  /// Some subtypes of [ScrollView] can infer this value automatically. For
+  /// example [ListView] will use the number of widgets in the child list,
+  /// while the [ListView.separated] constructor will use half that amount.
+  ///
+  /// For [CustomScrollView] and other types which do not receive a builder
+  /// or list of widgets, the child count must be explicitly provided. If the
+  /// number is unknown or unbounded this should be left unset or set to null.
+  ///
+  /// See also:
+  ///
+  ///  * [SemanticsConfiguration.scrollChildCount], the corresponding semantics
+  ///    property.
+  final int? semanticChildCount;
+
+  /// {@macro flutter.widgets.scrollable.dragStartBehavior}
+  final DragStartBehavior dragStartBehavior;
+
+  /// {@macro flutter.widgets.scroll_view.keyboardDismissBehavior}
+  ///
+  /// The default is [ScrollViewKeyboardDismissBehavior.manual]
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+
+  /// {@macro flutter.widgets.scrollable.restorationId}
+  final String? restorationId;
+
+  /// {@macro flutter.material.Material.clipBehavior}
+  ///
+  /// Defaults to [Clip.hardEdge].
+  final Clip clipBehavior;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      scrollDirection: Axis.vertical,
+      reverse: false,
+      controller: scrollController,
+      primary: primary,
+      physics: physics,
+      scrollBehavior: scrollBehavior,
+      shrinkWrap: shrinkWrap,
+      cacheExtent: cacheExtent,
+      semanticChildCount: semanticChildCount,
+      dragStartBehavior: dragStartBehavior,
+      keyboardDismissBehavior: keyboardDismissBehavior,
+      restorationId: restorationId,
+      clipBehavior: clipBehavior,
+      slivers: [
+        SliverPadding(
+          padding: padding ?? EdgeInsets.zero,
+          sliver: SliverTree<T>(
+            controller: controller,
+            itemBuilder: itemBuilder,
+            transitionBuilder: transitionBuilder,
+            maxNodesToShowWhenAnimating: maxNodesToShowWhenAnimating,
+          ),
+        ),
+      ],
+    );
+  }
+}
