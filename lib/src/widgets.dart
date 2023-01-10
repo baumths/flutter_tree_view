7f4fd4a83b1db5ffd27c8f5432ac73d594c3eb86
baumths <baumths@gmail.com>
2023-01-10 12:55:49 -0300

    remove TreeItem
    The TreeItem widget was only wrapping an InkWell + TreeIndentation
    whithout adding any new functionality.
    
    In the future, it would be replaced by "TreeTile", a custom render
    object similar to ListTile.

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -2,5 +2,4 @@ export 'widgets/drag_and_drop.dart';
 export 'widgets/folder_button.dart';
 export 'widgets/sliver_tree.dart';
 export 'widgets/tree_indentation.dart';
-export 'widgets/tree_item.dart';
 export 'widgets/tree_view.dart';
