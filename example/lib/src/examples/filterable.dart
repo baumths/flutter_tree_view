f6c573bd69c2e10485f5feb9b8d2a7c4b3056709
Danilo Maia Rodrigues <zidalo@gmail.com>
2024-09-23 12:48:35 -0300

    fix(example): hide Color from faker package

--- example/lib/src/examples/filterable.dart
+++ example/lib/src/examples/filterable.dart
@@ -1,4 +1,4 @@
-import 'package:faker/faker.dart';
+import 'package:faker/faker.dart' hide Color;
 import 'package:flutter/material.dart';
 import 'package:flutter_fancy_tree_view/flutter_fancy_tree_view.dart';
 
