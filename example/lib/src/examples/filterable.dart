f4a5cb2e6635f94c9909b1895089bf6167f55b1d
Balázs János Varga <vargab95@proton.me>
2024-10-05 11:04:14 +0200

    Fix: example build error

--- example/lib/src/examples/filterable.dart
+++ example/lib/src/examples/filterable.dart
@@ -1,4 +1,4 @@
-import 'package:faker/faker.dart';
+import 'package:faker/faker.dart' as faker;
 import 'package:flutter/material.dart';
 import 'package:flutter_fancy_tree_view/flutter_fancy_tree_view.dart';
 
@@ -256,7 +256,7 @@ class _TreeTileState extends State<TreeTile> {
   }
 }
 
-final lorem = Faker().lorem;
+final lorem = faker.Faker().lorem;
 void populateExampleTree(Node node, [int level = 0]) {
   if (level >= 7) return;
   node.children.addAll([
